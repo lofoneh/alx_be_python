44335a71b9203aad4e8feac212d8fdf794269b5a
Jeffery Lofoneh Asamani <jefferyasamani7@gmail.com>
2025-06-03 08:09:11 +0000

    chore: basic python practice.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum = number1 + number2
+# Subtraction
+difference = number1 - number2
+# Multiplication
+product = number1 * number2
+
+# results
+print("Additon of",number1, "and", number2,"is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
