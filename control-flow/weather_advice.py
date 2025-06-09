7c9bbbc3fa0f200ffd54d1c312cbc992f9aeb433
Jeffery Lofoneh Asamani <jefferyasamani7@gmail.com>
2025-06-09 08:34:25 +0000

    control: updated the weather program.

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,7 +3,7 @@ if User_response == "sunny":
     print("Wear a t-shirt and apply sunglasses.")
 elif User_response == "rainy":
     print ("Don't forget your umbrella and a raincoat.")
-elif User_response == "snowy":
+elif User_response == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
